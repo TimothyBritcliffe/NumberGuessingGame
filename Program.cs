30b830e852ba12eea2c40c99f4b33e8048980a67
Timothy Britcliffe <Timothy@britcliffe.com>
2026-04-15 22:46:19 -0700

    Added comments

--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        
+        //Variable Initialization (for the loop, random number, and the users guess)
         bool iter = true;
         
         Random random = new();
         int num = random.Next(1000);
         
-        Console.WriteLine("Guess the number between 0 and 999.");
         int guessNumber;
+        
+        
+        Console.WriteLine("Guess the number between 0 and 999.");
+
+        //Main game loop that runs until the users input matches the randomly generated number
         while (iter)
         {
             string guess = Console.ReadLine();
@@ -22,29 +27,34 @@ class Program
             {
                 guessNumber = Int32.Parse(guess);
             }
+            //If the input is not an integer
             catch (FormatException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Must input an integer.");
                 continue;
             }
+            //If the input is too big
             catch (OverflowException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Input too large.");
                 continue;
             }
+            //General catch for any other non-specific runtime errors
             catch  (Exception e)
             {
                 Console.WriteLine(e.Message);
                 continue;
             }
-
+            
+            
             iter = GameLogic(guessNumber, num);
         }
         
     }
 
+    //Game logic for comparing the guessNumber and random number (num); returns true or false depending on whether the user needs to make another guess or if the game is over
     private static bool GameLogic(int guessNumber, int num)
     {
         if (guessNumber == num)
